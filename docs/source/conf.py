ed4b04ea56cd75b807c5e895938a151f477df0ed
shenzhao <shenzhao9@huawei.com>
2026-05-18 15:10:38 +0800

    ci: update vllm commit pin for m2m

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,7 @@ myst_substitutions = {
     # CANN image tag
     "cann_image_tag": "9.0.0-910b-ubuntu22.04-py3.11",
     # vLLM commit hash for main branch
-    "main_vllm_commit": "0d4d334eaa583b9c09aa4eb7538c22db99fd84b3",
+    "main_vllm_commit": "990f49bdcb8ff51c0ceb1d784c3ca16e6c276927",
     # vLLM tag for main branch
     "main_vllm_tag": "v0.20.2",
     # Python version for main branch
