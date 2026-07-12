4b9e708b46e9f0f33a28d6dfd6d7d35ba919f2d3
zhao-stack <2020265299@qq.com>
2026-07-12 18:27:59 +0800

    Support legacy v0.24 speculative interfaces
    Signed-off-by: zhao-stack <2020265299@qq.com>

--- vllm_ascend/patch/worker/patch_v2/patch_triton.py
+++ vllm_ascend/patch/worker/patch_v2/patch_triton.py
@@ -10,7 +10,7 @@ from vllm_ascend.worker.v2.sample.gumbel import apply_temperature, gumbel_sample
 from vllm_ascend.worker.v2.sample.logprob import compute_token_logprobs, compute_topk_logprobs
 from vllm_ascend.worker.v2.sample.min_p import apply_min_p
 from vllm_ascend.worker.v2.sample.penalties import apply_penalties, bincount
-from vllm_ascend.worker.v2.spec_decode.dflash.speculator import _prepare_dflash_inputs_kernel_ascend
+from vllm_ascend.worker.v2.spec_decode.dflash.speculator import prepare_dflash_inputs_ascend
 from vllm_ascend.worker.v2.spec_decode.rejection_sampler_utils import (
     rejection_sample as npu_rejection_sample,
 )
@@ -34,4 +34,4 @@ logprob.compute_token_logprobs = compute_token_logprobs
 structured_outputs._apply_grammar_bitmask_kernel = _apply_grammar_bitmask_kernel
 rejection_sampler_utils.rejection_sample = npu_rejection_sample
 rejection_sampler.rejection_sample = npu_rejection_sample
-dflash_speculator._prepare_dflash_inputs_kernel = _prepare_dflash_inputs_kernel_ascend
+dflash_speculator.prepare_dflash_inputs = prepare_dflash_inputs_ascend
--- vllm_ascend/worker/v2/spec_decode/__init__.py
+++ vllm_ascend/worker/v2/spec_decode/__init__.py
@@ -31,7 +31,8 @@ def init_speculator(
     """
     speculative_config = vllm_config.speculative_config
     assert speculative_config is not None
-    if speculative_config.use_dspark():
+    use_dspark = getattr(speculative_config, "use_dspark", None)
+    if use_dspark is not None and use_dspark():
         from vllm_ascend.worker.v2.spec_decode.dspark.speculator import (
             AscendDSparkSpeculator,
         )
