7b0434419aee491620bb9d085273158307436007
zhao-stack <2020265299@qq.com>
2026-07-14 02:31:04 +0800

    Adapt runner interfaces for vLLM v0.24
    Signed-off-by: zhao-stack <2020265299@qq.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -5156,7 +5156,7 @@ class NPUModelRunner(GPUModelRunner):
                     min_cg_attn_backend = attn_backend.__name__
 
         with update_pass_config(self):
-            if vllm_version_is("0.23.0"):
+            if vllm_version_is("0.23.0") or vllm_version_is("0.24.0"):
                 cudagraph_mode = self.compilation_config.resolve_cudagraph_mode_and_sizes(
                     min_cg_support=min_cg_support,
                     min_cg_attn_backend=min_cg_attn_backend,
--- vllm_ascend/worker/v2/model_runner.py
+++ vllm_ascend/worker/v2/model_runner.py
@@ -18,6 +18,7 @@
 #
 
 from contextlib import contextmanager
+from typing import Any
 
 import numpy as np
 import torch
@@ -46,7 +47,7 @@ from vllm_ascend.ascend_forward_context import (
     set_mc2_tokens_capacity,
 )
 from vllm_ascend.ops.rotary_embedding import set_cos_and_sin, update_cos_sin
-from vllm_ascend.utils import set_weight_prefetch_method
+from vllm_ascend.utils import set_weight_prefetch_method, vllm_version_is
 from vllm_ascend.worker.v2.aclgraph_utils import ModelAclGraphManager
 from vllm_ascend.worker.v2.attn_utils import build_attn_state
 from vllm_ascend.worker.v2.input_batch import AscendInputBatch, AscendInputBuffers
@@ -326,7 +327,7 @@ class NPUModelRunner(GPUModelRunner):
             # max_seq_len is only consumed by the PP `compute_need_sampled_mask`.
             max_seq_len_np = self.req_states.max_seq_len[idx_mapping_np]
 
-        self.input_batch = AscendInputBatch(
+        input_batch_kwargs: dict[str, Any] = dict(
             req_ids=req_ids,
             num_reqs=num_reqs,
             num_reqs_after_padding=num_reqs_padded,
@@ -351,17 +352,20 @@ class NPUModelRunner(GPUModelRunner):
             max_seq_len_np=max_seq_len_np,
             input_ids=input_ids,
             positions=positions,
-            is_padding=self.input_buffers.is_padding[:num_tokens_after_padding],
             logits_indices=logits_indices,
             cu_num_logits=cu_num_logits,
             cu_num_logits_np=cu_num_logits_np,
             has_structured_output_reqs=scheduler_output.has_structured_output_requests,
-            # TODO: only populated for R-SWA (not supported yet).
-            prompt_lens=None,
             # extra attributes for ascend npus.
             seq_lens_np=self.input_buffers.seq_lens_np,
             attn_state=attn_state,
         )
+        if not (vllm_version_is("0.23.0") or vllm_version_is("0.24.0")):
+            # The verified main ref adds both fields; release InputBatch
+            # dataclasses do not define them.
+            input_batch_kwargs["is_padding"] = self.input_buffers.is_padding[:num_tokens_after_padding]
+            input_batch_kwargs["prompt_lens"] = None
+        self.input_batch = AscendInputBatch(**input_batch_kwargs)
 
         # For mla/sfa, update cos/sin. Here is for execute_model.
         update_cos_sin(self.input_batch.positions)
